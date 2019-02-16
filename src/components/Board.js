f5c88ec268e00dc547dcce34520372faed287ad0
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2019-02-16 15:41:36 +0100

    Taking Turns
    Alternate between crosses and circles.

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,8 +2,30 @@ import React from 'react';
 import Square from 'components/Square'
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
