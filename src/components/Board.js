5dc40e2d015bcba549705b17da56dce160041c43
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2019-02-21 07:54:27 +0100

    Lint code
    Done automatically with VSCode Tools.
    Remove trailing spaces.
    Use single quotes.
    No semicolon at the end of the line.
    Use a new line at the end of the file.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React from 'react'
 import Square from 'components/Square'
-import calculateWinner from 'helpers/gameLogic'
 
 export default class Board extends React.Component {
   renderSquare(i) {
@@ -9,28 +8,28 @@ export default class Board extends React.Component {
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
-    );
+    )
   }
 
   render() {
     return (
       <div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
       </div>
-    );
+    )
   }
 }
--- src/helpers/gameLogic.js
+++ src/helpers/gameLogic.js
@@ -7,13 +7,13 @@ export default function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
-  ];
+    [2, 4, 6]
+  ]
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+    const [a, b, c] = lines[i]
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return squares[a]
     }
   }
-  return null;
+  return null
 }
